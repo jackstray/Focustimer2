9ad53dd1a39e6a7668750cd1fca160436b9e0815
jackstray <borges2001jose@gmail.com>
2023-12-13 12:15:27 -0400

    corrigindo erro

--- FocusTimer2/src/main.js
+++
@@ -1,6 +0,0 @@
-// main.js
-import timer from "./timer.js";
-import { play, pause, increaseTime, decreaseTime } from "./controls.js";
-import { playSound } from "./sounds.js";
-
-// Adicione a lógica principal aqui, utilizando as funções importadas
